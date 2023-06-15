4386127ce5bc45aa5df17d1c091b363dbb3783a6
Maria <alece.1414@gmail.com>
2023-06-15 18:18:24 +0300

    Updates

--- src/components/pages/index.js
+++
@@ -1,8 +0,0 @@
-import MainPage from './mainPage/MainPage';
-import ComicsPage from './ComicsPage';
-import Page404 from './Page404';
-import SingleItem from './SingleItem';
-import SingleItemChar from './singleItemChar/SingleItemChar';
-import SingleItemComic from './singleItemComic/SingleItemComic';
-
-export {MainPage, ComicsPage, Page404, SingleItem, SingleItemChar, SingleItemComic};
